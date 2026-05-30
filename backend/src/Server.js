e3365ebf6f5ec05c7ab4421d8fcf6ccb3a232935
aashrith02 <aashiaashrith@gmail.com>
2026-05-30 23:14:27 +0530

    adding routes to express

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -1,50 +1,18 @@
 import express from "express";
 import cors from "cors";
-import { PrismaClient } from "@prisma/client";
+
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
-
-const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/api/auth/register", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    const existingUser = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-
-    if (existingUser) {
-      return res.status(400).json({
-        message: "User already exists",
-      });
-    }
-
-    const user = await prisma.user.create({
-      data: {
-        name,
-        email,
-        password,
-      },
-    });
-
-    res.status(201).json({
-      message: "User created",
-      user,
-    });
-  } catch (error) {
-    console.log(error);
-
-    res.status(500).json({
-      message: "Server error",
-    });
-  }
+app.get("/", (req, res) => {
+  res.send("Backend working");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(8000, () => {
   console.log("Server running on port 8000");
---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,48 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+});
+
+export default router;
